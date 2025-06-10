326b616b2413b8cf0ad5a38946131decbcbc0189
KafiyaNuril <kafiyanuril@gmail.com>
2025-06-10 11:10:43 +0700

    react

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -13,7 +13,7 @@ export default function Landing() {
     return (
          <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
             <div className="text-center p-5 bg-white rounded shadow" style={{ maxWidth: "500px", width: "100%" }}>
-                <h1 className="mb-4 fw-bold text-dark">Welcome to Our Library App</h1>
+                <h1 className="mb-4 fw-bold" style={{ color: "#17a2b8"}}>Welcome to Our Library App</h1>
                 <p className="mb-4 text-muted fw-semibold">Manage your library easily and efficiently.</p>
                 <div className="d-flex justify-content-center gap-3">
                     <button className="btn px-4 text-white fw-semibold" style={{ backgroundColor: "#17a2b8"}} onClick={handleLogin}>
